772ec4b97ac0ae3eb6f6aef93f06a2a71446436c
Guido Lorenz <code@superquadratic.net>
2013-02-27 11:50:52 +0100

    Move stoplight definition into spec scope

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -2,16 +2,15 @@ require("busted")
 
 local machine = require("statemachine")
 
-local stoplight = {
-  { name = 'warn',  from = 'green',  to = 'yellow' },
-  { name = 'panic', from = 'yellow', to = 'red'    },
-  { name = 'calm',  from = 'red',    to = 'yellow' },
-  { name = 'clear', from = 'yellow', to = 'green'  }
-}
-
 describe("Lua state machine framework", function()
   describe("A stop light", function()
     local fsm
+    local stoplight = {
+      { name = 'warn',  from = 'green',  to = 'yellow' },
+      { name = 'panic', from = 'yellow', to = 'red'    },
+      { name = 'calm',  from = 'red',    to = 'yellow' },
+      { name = 'clear', from = 'yellow', to = 'green'  }
+    }
 
     before_each(function()
       fsm = machine.create({ initial = 'green', events = stoplight })
